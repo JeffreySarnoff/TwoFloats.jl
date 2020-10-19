5f4899b41405ecae4a578ef2028e31f93ef9fd83
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-19 01:01:51 -0400

    Update pair_arithmetic.jl

--- src/pair_arithmetic.jl
+++ src/pair_arithmetic.jl
@@ -1,3 +1,9 @@
+Base.:(-)(a::Pair) = (-a[1], -a[2])
+Base.signbit(a::Pair) = signbit(a[1])
+Base.abs(a::Pair) = signbit(a[1]) ? (abs(a[1]), -a[2]) : a
+Base.copysign(a::Pair, b) = signbit(b) ? (signbit(a[1]) ? a : -a) : a
+Base.flipsign(a::Pair, b) = signbit(b) ? -a : a
+
 function Base.:(+)(a::Pair, b::Pair)
     ahi, alo = a
     bhi, blo = b
@@ -52,11 +58,18 @@ function Base.inv(b::Pair{T}) where {T}
     return Pair((hi, lo))
 end
 
+function Base.sqrt(a::Pair)
+    ahi, alo = a
+    hi = sqrt(ahi)
+    t = fma(hi, -hi, ahi)
+    lo = (t + alo) / (2 * hi)
+    return Pair((hi, lo))
+end
 
 #=
     These function definitions appear on page 4 of "Faithfully Rounded Floating-point Computations"
 =#
-
+#=
 function pair_sum(ae::Pair, bf::Pair)
     a, e = ae
     b, f = bf
@@ -100,4 +113,4 @@ function pair_sqrt(ae::Pair)
     lo = (t + e) / (hi + hi)
     return Pair((hi, lo))
 end
-
+=#
