6d5f32e0b48c733fc013627bc303cc03fd8cdacc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-19 04:30:37 -0400

    Update paired.jl

--- src/paired.jl
+++ src/paired.jl
@@ -12,5 +12,6 @@ Pair{T}(p::Pair{T}) where {T} = p
 Pair(p::Pair{T}) where {T} = p
 Pair{T1}(p::Pair{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
 
-renorm(a::Pair{T}) where {T} = two_hilo_sum(a[1], a[2])
+
 refine(a::Pair{T}) where {T} = two_sum(a[1], a[2])
+renorm(a::Pair{T}) where {T} = two_hilo_sum(a[1], a[2])
