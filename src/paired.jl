082aa324b05f9db71a276c49bfae62c2977dc85c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-19 01:13:50 -0400

    Update paired.jl

--- src/paired.jl
+++ src/paired.jl
@@ -1,5 +1,3 @@
-# type
-
 """
     Pair{T} = Tuple{T,T}
 
@@ -7,9 +5,12 @@ Pair(a, b) == (maxabs(a,b), minabs(a,b))
 """
 const Pair{T} = Tuple{T,T} where {T}
 
-Pair{T}(a::T, b::T) where {T} = abs(a) < abs(b) ? (b, a) : (a, b)
+Pair{T}(a::T, b::T) where {T} = abs(a) < abs(b) ? two_hilo_sum(b, a) : two_hilo_sum(a, b)
 Pair(a::T, b::T) where {T} = Pair{T}(a, b)
 
-Pair{T}(ab::Pair{T}) where {T} = ab
-Pair(ab::Pair{T}) where {T} = ab
-Pair{T1}(ab::Pair{T2}) where {T1,T2} = (T1(a), T1(b))
+Pair{T}(p::Pair{T}) where {T} = p
+Pair(p::Pair{T}) where {T} = p
+Pair{T1}(p::Pair{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
+
+refine(a::Pair{T}) where {T} = two_hilo_sum(a[1], a[2])
+repair(a::Pair{T}) where {T} = two_sum(a[1], a[2])
