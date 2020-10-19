463a99d185d33cae6f4f74f3420a447d59e4db9f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-18 23:04:54 -0400

    Update paired.jl

--- src/paired.jl
+++ src/paired.jl
@@ -1,3 +1,12 @@
 # type
 
+"""
+    Pair{T} = Tuple{T,T}
+
+Pair(a, b)
+"""
 const Pair{T} = Tuple{T,T} where {T}
+
+Pair{T}(a::T, b::T) where {T} = abs(a) < abs(b) ? (b, a) : (a, b)
+Pair(a::T, b::T) where {T} = Pair{T}(a, b)
+
