488612bf381ad69e5ec6291a704c1118a2d2dfcc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-18 23:06:19 -0400

    Update paired.jl

--- src/paired.jl
+++ src/paired.jl
@@ -10,3 +10,4 @@ const Pair{T} = Tuple{T,T} where {T}
 Pair{T}(a::T, b::T) where {T} = abs(a) < abs(b) ? (b, a) : (a, b)
 Pair(a::T, b::T) where {T} = Pair{T}(a, b)
 
+Pair(ab::Pair{T}) where {T} = ab
