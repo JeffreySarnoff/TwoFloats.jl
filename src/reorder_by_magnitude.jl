0b78853c61a9a5dbc515a2b26e6a6c7656f0b9eb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-20 00:37:16 -0400

    Update reorder_by_magnitude.jl

--- src/reorder_by_magnitude.jl
+++ src/reorder_by_magnitude.jl
@@ -5,9 +5,32 @@
 
       maxmin_abs <-- reorder by decreasing absolute value
       minmax_abs <-- reorder by increasing absolute value
+
+      hitolo_abs <-- reorder by decreasing absolute value of first-in-pair 
       mxxmnn_abs <-- reorder pairwise by decreasing absolute value
       
 =#
+
+"""
+    minmax_abs(a, b)
+    
+- assures abs(a) <= abs(b)
+    - where a, b = minmax_abs(a, b)
+"""
+minmax_abs(a::T, b::T) where {T} = abs(b) < abs(a) ? (b, a) : (a, b)
+
+"""
+    minmax_abs(a, b, c)
+    
+- assures abs(a) <= abs(b) <= abs(c)
+    - where a, b, c = minmax_abs(a, b, c)
+"""
+function minmax_abs(a::T, b::T, c::T) where {T}
+    b, c = minmax_abs(b, c)
+    a, c = minmax_abs(a, c)
+    a, b = minmax_abs(a, b)
+    return (a, b, c)
+end
 
 """
     maxmin_abs(a, b)
@@ -31,6 +54,20 @@ function maxmin_abs(a::T, b::T, c::T) where {T}
 end
 
 """
+    hitolo_abs(ahi, alo, bhi, blo)
+    
+- assures abs(ahi) >= abs(bhi) 
+    - where ahi, alo, bhi, blo = mxmn_abs(ahi, alo, bhi, blo)
+"""
+function hi2lo_abs(ahi, alo, bhi, blo)
+      if abs(ahi) < abs(bhi)
+         bhi, blo, ahi, alo
+      else
+         ahi, alo, bhi, blo
+      end
+end   
+
+"""
     mxxmnn_abs(ahi, alo, bhi, blo)
     
 - assures abs(ahi) >= abs(bhi) >= abs(alo) >= abs(blo)
@@ -43,24 +80,4 @@ function mxxmnn_abs(ahi::T, alo::T, bhi::T, blo::T) where {T}
     return (ahi, alo, bhi, blo)
 end
 
-"""
-    minmax_abs(a, b)
-    
-- assures abs(a) <= abs(b)
-    - where a, b = minmax_abs(a, b)
-"""
-minmax_abs(a::T, b::T) where {T} = abs(b) < abs(a) ? (b, a) : (a, b)
-
-"""
-    minmax_abs(a, b, c)
-    
-- assures abs(a) <= abs(b) <= abs(c)
-    - where a, b, c = minmax_abs(a, b, c)
-"""
-function minmax_abs(a::T, b::T, c::T) where {T}
-    b, c = minmax_abs(b, c)
-    a, c = minmax_abs(a, c)
-    a, b = minmax_abs(a, b)
-    return (a, b, c)
-end
 
