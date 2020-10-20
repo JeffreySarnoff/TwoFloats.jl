4694d7ca5233079a9e64b8d3a99ebb25580df318
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-19 23:43:05 -0400

    Update typed_pairs.jl

--- src/typed_pairs.jl
+++ src/typed_pairs.jl
@@ -14,12 +14,12 @@ Pair{T1}(p::Pair{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
 
 
 """
-    refine(a::Pair)
+    refresh(a::Pair)
 
 resets the canonical form
 - a = refine(a)
 """
-refine(a::Pair{T}) where {T} = two_sum(a[1], a[2])
+refresh(a::Pair{T}) where {T} = two_sum(a[1], a[2])
 
 """
     renorm(a::Pair)
