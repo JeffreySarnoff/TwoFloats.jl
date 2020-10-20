cb508f0f340b67558ee900385cf55d74ae1a05b9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-20 00:51:31 -0400

    Update pair_arithmetic.jl

--- src/pair_arithmetic.jl
+++ src/pair_arithmetic.jl
@@ -4,10 +4,36 @@ Base.abs(a::Pair) = signbit(a[1]) ? (abs(a[1]), -a[2]) : a
 Base.copysign(a::Pair, b) = signbit(b) ? (signbit(a[1]) ? a : -a) : a
 Base.flipsign(a::Pair, b) = signbit(b) ? -a : a
 
+function Base.:(+)(a::Pair{T}, b::Pair{T}) where {T}
+    if abs(b[1]) <= abs(a[1])
+         ahi, alo = a
+         bhi, blo = b
+    else
+         ahi, alo = b
+         bhi, blo = a
+    end
+    return Base.:(+)(ahi, alo, bhi, blo)
+end
+
+function Base.:(+)(ahi::T, alo::T, bhi::T, blo::T)  where {T}
+    t0, t1 = two_hilo_sum(ahi, alo)
+    t2, t3 = two_hilo_sum(bhi, blo)
+    hi, t4 = two_hilo_sum(t0, t2)
+    lo = t4 + (t1 + t3)
+    hi, lo = two_hilo_sum(hi, lo)
+    
+    return Pair((hi, lo))
+end
+
+
 function Base.:(+)(a::Pair, b::Pair)
-    ahi, alo = a
-    bhi, blo = b
-    ahi, alo, bhi, blo = mxxmnn_abs(ahi, alo, bhi, blo)
+    if abs(b[1]) <= abs(a[1])
+         ahi, alo = a
+         bhi, blo = b
+    else
+         ahi, alo = b
+         bhi, blo = a
+    end
     t0, t1 = two_hilo_sum(ahi, alo)
     t2, t3 = two_hilo_sum(bhi, blo)
     hi, t4 = two_hilo_sum(t0, t2)
@@ -15,6 +41,21 @@ function Base.:(+)(a::Pair, b::Pair)
     hi, lo = two_hilo_sum(hi, lo)
     return Pair((hi, lo))
 end
+
+#=
+function Base.:(+)(a::Pair, b::Pair)
+    ahi, alo = a
+    bhi, blo = b
+    # ahi, alo, bhi, blo = mxxmnn_abs(ahi, alo, bhi, blo)
+    ahi, alo, bhi, blo = hitolo_abs(ahi, alo, bhi, blo)
+    t0, t1 = two_hilo_sum(ahi, alo)
+    t2, t3 = two_hilo_sum(bhi, blo)
+    hi, t4 = two_hilo_sum(t0, t2)
+    lo = t4 + (t1+t3)
+    hi, lo = two_hilo_sum(hi, lo)
+    return Pair((hi, lo))
+end
+=#
 
 function Base.:(-)(a::Pair, b::Pair)
     ahi, alo = a
