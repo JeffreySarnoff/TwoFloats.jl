1b7e493b23d8dda093b9d09fc701d975ce8e2646
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 08:08:05 -0400

    Update typed_pairs.jl

--- src/typed_pairs.jl
+++ src/typed_pairs.jl
@@ -8,32 +8,115 @@ Float96(x::Float64, y::Float64) = two_sum(x, y)
 Float48(x::Float24, y::Float24) = two_sum(x, y)
 
 #=
-FloatFloat{T} = Tuple{T,T}
-FloatFloat(a, b) == (maxabs(a,b), minabs(a,b))
-=#
+    TwoFloat{T}(a, b) --> (a, b)
 
-const FloatFloat{T} = Tuple{T,T} where {T}
+    TwoFloat( a, b )  --> two_sum(a, b)
+    TwoFloat((a, b))  --> two_hilo_sum(a, b)
+=#    
 
-FloatFloat{T}(a::T, b::T) where {T} =
-    abs(a) < abs(b) ? two_hilo_sum(b, a) : two_hilo_sum(a, b)
-FloatFloat(a::T, b::T) where {T} = FloatFloat{T}(a, b)
+struct TwoFloat{T} <: AbstractFloat
+    hi::T
+    lo::T
 
-FloatFloat{T}(p::FloatFloat{T}) where {T} = p
-FloatFloat(p::FloatFloat{T}) where {T} = p
-FloatFloat{T1}(p::FloatFloat{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
+    function TwoFloat{T}(a::T, b::T) where {T}
+        return new{T}(a,b)
+    end
+end
+
+function TwoFloat(a::T, b::T) where {T}
+    a, b = two_sum(a, b)
+    return TwoFloat{T}(a, b)
+end            
+
+function TwoFloat(ab::Tuple{T,T}) where {T}
+    a, b = two_hilo_sum(a, b)
+    return TwoFloat{T}(a, b)
+end
+
+
+
+
+@inline function two_sum(a::T, b::T) where {T}
+   hi = a + b
+   v  = hi - a
+   lo = (a - (hi - v)) + (b - v)
+   return hi, lo
+end
+
+@inline function two_hilo_sum(a::T, b::T) where {T}
+   hi = a + b
+   lo = b - (hi - a)
+   return hi, lo
+end
+
+@inline function two_div(a::T, b::T) where {T}
+    hi = a / b
+    lo = fma(-hi, b, a)
+    lo /= b
+    return hi, lo
+end
+
+@inline function two_prod(a::T, b::T) where {T}
+   hi = a * b
+   lo = fma(a, b, -hi)
+   hi, lo
+end
+
+
+hash(x::TwoFloat, h::UInt) = hash(x.lo, hash(x.hi, h))
+
+eltype(x::Type{TwoFloat{T}}) where {T} = T}
+
+length(x::TwoFloat{T}) where {T} = 2
+
+iterate(x::TwoFloat, i=1) = i > 2 ? nothing : (getfield(p, i), i + 1)
+indexed_iterate(x::TwoFloat, i::Int, state=1) = (getfield(p, i), i + 1)
+
+
+==(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi === y.hi) & (x.lo === y.lo)
+
+!=(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi !=== y.hi) | (x.lo !=== y.lo)
+
+<=(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi < y.hi) || ((x.hi === y.hi) & (x.lo <= y.lo))
+
+>=(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi > y.hi) || ((x.hi === y.hi) & (x.lo >= y.lo))
+
+<(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi < y.hi) || ((x.hi === y.hi) & (x.lo < y.lo))
+
+>(x::TwoFloat{T}, y::TwoFloat{T}) where {T} =
+   (x.hi > y.hi) || ((x.hi === y.hi) & (x.lo > y.lo))
+
+isequal(x::TwoFloat, y::TwoFloat) = 
+    isequal(x.hi ,y.hi) & isequal(x.lo, y.lo)
+
+isless(x::TwoFloat, y::TwoFloat) = 
+    ifelse(!isequal(x.hi,y.hi), isless(x.hi,y.hi), isless(x.lo,y.lo))
+
+getindex(x::TwoFloat, i::Int) = getfield(p,i)
+getindex(x::TwoFloat, i::Real) = getfield(p, convert(Int, i))
+
+firstindex(x::TwoFloat{T}) where {T} = 1
+lastindex(x::TwoFloat{T})  where {T} = 2
+first(x::TwoFloat{T}) where {T} = x.hi
+last(x::TwoFloat{T})  where {T} = x.lo
 
 """
-    refresh(a::FloatFloat)
+    refresh(a::TwoFloat)
 
 resets the canonical form
 - a = refine(a)
 """
-refresh(a::FloatFloat{T}) where {T} = two_sum(a[1], a[2])
+refresh(a::TwoFloat{T}) where {T} = two_sum(a.hi, a.lo)
 
 """
-    renorm(a::FloatFloat)
+    renorm(a::TwoFloat)
 
 *unchecked assumption*: abs(a[1]) >= abs(a[2])
 - a = renorm(a)
 """
-renorm(a::FloatFloat{T}) where {T} = two_hilo_sum(a[1], a[2])
+renorm(a::TwoFloat{T}) where {T} = two_hilo_sum(a.hi, a.lo)
