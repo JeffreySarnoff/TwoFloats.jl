425f34ac714edd808f8956e8e5b4a58de715aed6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 03:36:50 -0400

    Update typed_pairs.jl

--- src/typed_pairs.jl
+++ src/typed_pairs.jl
@@ -1,30 +1,30 @@
+#=
+FloatFloat{T} = Tuple{T,T}
+FloatFloat(a, b) == (maxabs(a,b), minabs(a,b))
+=#
+
+const FloatFloat{T} = Tuple{T,T} where {T}
+
+FloatFloat{T}(a::T, b::T) where {T} =
+    abs(a) < abs(b) ? two_hilo_sum(b, a) : two_hilo_sum(a, b)
+FloatFloat(a::T, b::T) where {T} = FloatFloat{T}(a, b)
+
+FloatFloat{T}(p::FloatFloat{T}) where {T} = p
+FloatFloat(p::FloatFloat{T}) where {T} = p
+FloatFloat{T1}(p::FloatFloat{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
+
 """
-    Pair{T} = Tuple{T,T}
-
-Pair(a, b) == (maxabs(a,b), minabs(a,b))
-"""
-const Pair{T} = Tuple{T,T} where {T}
-
-Pair{T}(a::T, b::T) where {T} = abs(a) < abs(b) ? two_hilo_sum(b, a) : two_hilo_sum(a, b)
-Pair(a::T, b::T) where {T} = Pair{T}(a, b)
-
-Pair{T}(p::Pair{T}) where {T} = p
-Pair(p::Pair{T}) where {T} = p
-Pair{T1}(p::Pair{T2}) where {T1,T2} = (T1(p[1]), T1(p[2]))
-
-
-"""
-    refresh(a::Pair)
+    refresh(a::FloatFloat)
 
 resets the canonical form
 - a = refine(a)
 """
-refresh(a::Pair{T}) where {T} = two_sum(a[1], a[2])
+refresh(a::FloatFloat{T}) where {T} = two_sum(a[1], a[2])
 
 """
-    renorm(a::Pair)
+    renorm(a::FloatFloat)
 
 *unchecked assumption*: abs(a[1]) >= abs(a[2])
 - a = renorm(a)
 """
-renorm(a::Pair{T}) where {T} = two_hilo_sum(a[1], a[2])
+renorm(a::FloatFloat{T}) where {T} = two_hilo_sum(a[1], a[2])
