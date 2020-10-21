881fb4d4a4ec0de175bf8aabbb13886895e9fa6d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 03:32:12 -0400

    Update PairArithmetic.jl

--- src/PairArithmetic.jl
+++ src/PairArithmetic.jl
@@ -1,6 +1,7 @@
 module PairArithmetic
+#module FloatFloats
 
-export Paired
+export FloatFloat
 
 const FastFloat = Union{Float32, Float64}
 
@@ -8,8 +9,8 @@ using LinearAlgebra
 using StructArrays, LoopVectorization
 using ErrorfreeArithmetic
 
-include("typed_pairs.jl")
-include("pair_arithmetic.jl")
+include("typed_floatfloat.jl")
+include("floatfloat_arithmetic.jl")
 include("reorder_by_magnitude.jl")
 
-end  # PairArithmetic
+end # module
