8a89717d31d1a17d005fab483e1776a558fc4fab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 03:37:51 -0400

    Update pair_arithmetic.jl

--- src/pair_arithmetic.jl
+++ src/pair_arithmetic.jl
@@ -1,10 +1,10 @@
-Base.:(-)(a::Pair) = (-a[1], -a[2])
-Base.signbit(a::Pair) = signbit(a[1])
-Base.abs(a::Pair) = signbit(a[1]) ? (abs(a[1]), -a[2]) : a
-Base.copysign(a::Pair, b) = signbit(b) ? (signbit(a[1]) ? a : -a) : a
-Base.flipsign(a::Pair, b) = signbit(b) ? -a : a
+Base.:(-)(a::FloatFloat) = (-a[1], -a[2])
+Base.signbit(a::FloatFloat) = signbit(a[1])
+Base.abs(a::FloatFloat) = signbit(a[1]) ? (abs(a[1]), -a[2]) : a
+Base.copysign(a::FloatFloat, b) = signbit(b) ? (signbit(a[1]) ? a : -a) : a
+Base.flipsign(a::FloatFloat, b) = signbit(b) ? -a : a
 
-function Base.:(+)(a::Pair{T}, b::Pair{T}) where {T}
+function Base.:(+)(a::FloatFloat{T}, b::FloatFloat{T}) where {T}
     if abs(b[1]) <= abs(a[1])
          ahi, alo = a
          bhi, blo = b
@@ -22,11 +22,11 @@ function Base.:(+)(ahi::T, alo::T, bhi::T, blo::T)  where {T}
     lo = t4 + (t1 + t3)
     hi, lo = two_hilo_sum(hi, lo)
     
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
 
-function Base.:(+)(a::Pair, b::Pair)
+function Base.:(+)(a::FloatFloat, b::FloatFloat)
     if abs(b[1]) <= abs(a[1])
          ahi, alo = a
          bhi, blo = b
@@ -39,11 +39,11 @@ function Base.:(+)(a::Pair, b::Pair)
     hi, t4 = two_hilo_sum(t0, t2)
     lo = t4 + (t1+t3)
     hi, lo = two_hilo_sum(hi, lo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
 #=
-function Base.:(+)(a::Pair, b::Pair)
+function Base.:(+)(a::FloatFloat, b::FloatFloat)
     ahi, alo = a
     bhi, blo = b
     # ahi, alo, bhi, blo = mxxmnn_abs(ahi, alo, bhi, blo)
@@ -53,11 +53,11 @@ function Base.:(+)(a::Pair, b::Pair)
     hi, t4 = two_hilo_sum(t0, t2)
     lo = t4 + (t1+t3)
     hi, lo = two_hilo_sum(hi, lo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 =#
 
-function Base.:(-)(a::Pair, b::Pair)
+function Base.:(-)(a::FloatFloat, b::FloatFloat)
     ahi, alo = a
     bhi, blo = b
     ahi, alo, bhi, blo = mxxmnn_abs(ahi, alo, -bhi, -blo)
@@ -66,10 +66,10 @@ function Base.:(-)(a::Pair, b::Pair)
     hi, t4 = two_hilo_sum(t0, t2)
     lo = t4 + (t1+t3)
     hi, lo = two_hilo_sum(hi, lo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
-function Base.:(*)(a::Pair, b::Pair)
+function Base.:(*)(a::FloatFloat, b::FloatFloat)
     ahi, alo = a
     bhi, blo = b
     if abs(alo) > abs(bhi)
@@ -79,30 +79,30 @@ function Base.:(*)(a::Pair, b::Pair)
     hi = ahi * bhi
     t = fma(ahi, bhi, -hi)
     lo = t + (ahi*blo + bhi*alo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
-function Base.:(/)(a::Pair, b::Pair)
+function Base.:(/)(a::FloatFloat, b::FloatFloat)
     ahi, alo = a
     bhi, blo = b
     hi = ahi / bhi
     t = fma(-bhi, hi, ahi)
     lo = ((t + alo) - hi*blo) / (bhi + blo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
-function Base.inv(b::Pair{T}) where {T}
+function Base.inv(b::FloatFloat{T}) where {T}
     bhi, blo = b
     hi = inv(bhi)
     t = fma(-bhi, hi, one(T))
     lo = -(hi * blo) / (bhi + blo)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
 
-function Base.sqrt(a::Pair)
+function Base.sqrt(a::FloatFloat)
     ahi, alo = a
     hi = sqrt(ahi)
     t = fma(hi, -hi, ahi)
     lo = (t + alo) / (2 * hi)
-    return Pair((hi, lo))
+    return FloatFloat((hi, lo))
 end
