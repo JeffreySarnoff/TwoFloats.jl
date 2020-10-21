078aee2b9de54b1f9a164c4a3630c3969aecdc86
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 08:56:56 -0400

    Update structarray.jl

--- src/structarray.jl
+++ src/structarray.jl
@@ -16,5 +16,25 @@ function StaticArrays.createinstance(::Type{Pair{T}}, hi::T, lo::T) where {T}
     renorm(hi, lo)
 end
 
+using StructArrays
 
+
+
+Base.getproperty(x::TwoFloat, s::Symbol) = 
+    (s == :hi ? getfield(x, 1) :
+    (s == :lo ? getfield(x, 2) :
+          error("($s) is not a field of `TwoFloat`.")))
+
+Base.propertynames(b::TwoFloat) = (:hi, :lo) 
+
+# explicitly give the "schema" of the object to StructArrays
+
+function StructArrays.staticschema(::Type{TwoFloat{T}}) where {T}}
+    TwoFloat(::TwoFloat{(:hi, :lo), Base.tuple_type_cons(T, types)}
+end
+
+# generate an instance of TwoFloat type
+function StructArrays.createinstance(::Type{TwoFloat{T}}, x, args...) where {T}
+    TwoFloat(x, T(args)
+end
     
