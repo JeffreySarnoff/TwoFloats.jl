c927721282452db96595269c7f2ae12a955bc557
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-21 21:52:25 -0400

    Update TwoFloats.jl

--- src/TwoFloats.jl
+++ src/TwoFloats.jl
@@ -9,13 +9,12 @@ using StructArrays, LoopVectorization
 using ErrorfreeArithmetic
 
 include("float_struct.jl")
-const Float96 = Floats{2, Float64}
-const Float48 = Floats{2, Float32}
+const Float96 = TwoFloat{Float64}
+const Float48 = TwoFloat{Float32}
 
 inclued("support_StructArrays.jl")
-
 
 include("floatfloat_arithmetic.jl")
 include("reorder_by_magnitude.jl")
 
-end  TwoFloats
+end  # TwoFloats
